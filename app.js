771752467170527a24388aea5e274c78c64d7d08
george-of-croton <armstronggeorge10@gmail.com>
2017-03-22 16:12:15 +1100

    make it so

---
+++ app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const Hapi = require('hapi');
+const scrape = require('./index.js')
+const promise = require('promise')
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({
+	host: 'localhost',
+	port: 8000
+});
+
+// Add the route
+server.register(require('inert'), (err) => {
+
+	if (err) {
+		throw err;
+	}
+
+	server.route({
+		method: 'GET',
+		path: '/hello',
+		handler: function(request, reply) {
+			reply.file('./home.html');
+		}
+	});
+
+	server.route({
+		method: 'GET',
+		path: '/{file*}',
+		handler: {
+			directory: {
+				path: './',
+				listing: true
+			}
+		}
+	});
+});
+
+server.route({
+	method: 'GET',
+	path: '/scrape',
+	handler: function(request, reply) {
+		scrape(1, function(err, data) {
+			reply(JSON.stringify({
+				"data": data
+			}))
+		})
+	}
+})
+
+// Start the server
+server.start((err) => {
+	if (err) {
+		throw err;
+	}
+	console.log('Server running at:', server.info.uri);
+});
--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 var $ = require('cheerio')
 var request = require('request')
-var inc = 0;
+var fs = require('fs')
 var mastercoords = []
 
 
 
+module.exports = function scrape(inc, callback) {
+	var counter = 0
+	var container = []
+	var url = 'http://www.realcommercial.com.au/for-sale/in-melbourne/list-' + inc
+	request.get(url, function(req, res, body) {
 
-function scrape() {
-	while (inc < 30) {
-		var container = []
-		var url = 'http://www.realcommercial.com.au/for-sale/in-melbourne/list-' + inc + '?nearbySuburb=false&autoSuggest=true'
-		request.get(url, function(req, res, body) {
-			container.push($('script', body))
-			container.forEach(function(x) {
-				var keys = Object.keys(x)
-				var ele = x[12].children[0].data.split(x[12].children[0].data.indexOf('['), x[12].children[0].data.length - 1)
-				var data = ele[ele.length - 1].split(',')
 
-				var coords = []
-				data.forEach(function(y) {
-					if (coords.length == 2) {
-						mastercoords.push(coords)
-						coords = []
-					}
+		container.push($('script', body))
+		container.forEach(function(x) {
+			if (inc > 4 && typeof callback == 'function') {
+				return callback(mastercoords)
+			}
+			var keys = Object.keys(x)
+			var ele = x[12].children[0].data.split(x[12].children[0].data.indexOf('['), x[12].children[0].data.length - 1)
+			var data = ele[ele.length - 1].split(',')
+
+			var coords = []
+
+			data.forEach(function(y) {
+				counter++
+				if (counter === data.length && typeof callback == "function") {
+					callback(null, mastercoords)
+				}
+				if (coords.length == 2) {
+					mastercoords.push(coords)
+					coords = []
+				} else {
 					var item = y.split(":")
 					if (item[0] == "latitude" || item[0] == "longitude") {
-
 						coords.push(item[1])
 					}
-
-				})
-				console.log(mastercoords)
+				}
 			})
 		})
-
-		inc++
-		scrape()
-	}
+	})
 }
-
-
-scrape()
